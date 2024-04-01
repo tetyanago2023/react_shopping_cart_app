e609cf027037b1e1b8cd97d605c0d7d8e7d0daa9
go2023 <tetyanago2018@gmail.com>
2024-03-31 17:12:54 -0700

    components were created, tailwind was configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/cart";
+import Home from "./pages/home";
+import Header from "./components/header";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
+
