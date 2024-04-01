2d2329c8bfa5716704e6f3e3a34c7c30e9f9381b
go2023 <tetyanago2018@gmail.com>
2024-03-31 23:45:10 -0700

    Cart was adjusted

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -14,26 +14,46 @@
 //
 // export default Cart;
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-    const[totalCart, serTotalCart] = useState(0)
-    // Select specific parts of the state that your component needs
-    const cartItems = useSelector(state => state.cart); // Assuming 'cart' is the slice name
+    const[totalCart, setTotalCart] = useState(0)
+    const cart = useSelector(state => state.cart);
+
+    //
 
     useEffect(() => {
-        serTotalCart(cartItems.reduce((acc, curr) => acc + curr.price, 0))
-    }, [cartItems]);
+        // Ensure cart is an array before trying to calculate the total price
+        if (Array.isArray(cart) && cart.length > 0) {
+            // Use reduce to sum up the prices of all items in the cart
+            const totalPrice = cart.reduce((acc, curr) => acc + curr.price);
+            setTotalCart(totalPrice);
+        } else {
+            // If cart is empty or not an array, set totalCart to 0
+            setTotalCart(0);
+        }
+    }, [cart]);
 
-    console.log(cartItems,totalCart);
+    console.log(cart,totalCart);
 
-    return (
-        <div>
-            <h2>Cart</h2>
-            {/* Render your cart items here */}
-        </div>
-    );
+    return <>
+        {
+            cart && cart.length
+                ? <div>
+
+                </div>
+                : <div className="min-h-[80vh] flex flex-col items-center justify-center">
+                    <h1 className="text-gray-800 font-bold text-xl mb-2">Your cart is empty</h1>
+                <Link to={"/"}>
+                    <button className="bg-red-950 text-white boder-2 rounded-lg font-bold p-4">
+                        Shop now
+                    </button>
+                </Link>
+                </div>
+        }
+    </>
 };
 
 export default Cart;
